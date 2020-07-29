b31878baa96dd8b3756333e2aa076fcc49a1fd09
josephtmartin <goldglovejo13@gmail.com>
2020-07-28 20:34:38 -0500

    added use strict

--- setup/app.js
+++ setup/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const handleButtonClickHex = () => {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   document.body.style.backgroundColor = `#${randomColor}`;
