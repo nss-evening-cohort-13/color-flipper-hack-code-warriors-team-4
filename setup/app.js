94f725fa7d8d781c480dd2a62b0ccc9dd60af2b7
Ryan McNair <ryan.mcnair@gmail.com>
2020-07-28 19:48:11 -0500

    updated app.js handleButtonClickHex funtion name

--- setup/app.js
+++ setup/app.js
@@ -1,10 +1,10 @@
-const handleButtonClick = () => {
+const handleButtonClickHex = () => {
     const randomColor = Math.floor(Math.random()*16777215).toString(16);
     document.body.style.backgroundColor = "#" + randomColor;
   };
   
   const buttonEvent = () => {
-      document.querySelector('#button').addEventListener("click", handleButtonClick)
+      document.querySelector('#button').addEventListener("click", handleButtonClickHex)
   };
 
   buttonEvent();
